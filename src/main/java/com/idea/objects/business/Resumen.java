4a3b1bdfaa6fc0247390edb3cf04725a4b413aeb
Julio Acosta <julio.acosta.cota@gmail.com>
2023-03-09 14:39:53 -0700

    Se pusieron los listados proveedor/obra en el resumen

---
+++ src/main/java/com/idea/objects/business/Resumen.java
@@ -0,0 +1,23 @@
+package com.idea.objects.business;
+
+
+
+public class Resumen {
+	private String concepto;
+	private Double importe;
+	
+
+	public String getConcepto() {
+		return concepto;
+	}
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
+	public Double getImporte() {
+		return importe;
+	}
+	public void setImporte(Double importe) {
+		this.importe = importe;
+	}
+	
+}
