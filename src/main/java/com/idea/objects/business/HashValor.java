bdeef1507e883cc5a1a5c6f601fba75a3ddd5f0e
Julio Acosta <julio.acosta.cota@gmail.com>
2023-03-31 14:05:58 -0700

    Se agrego clase hashValor

---
+++ src/main/java/com/idea/objects/business/HashValor.java
@@ -0,0 +1,30 @@
+package com.idea.objects.business;
+
+
+public class HashValor {
+	
+	private Integer id;	
+	private Double valor;
+	
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public Double getValor() {
+		return valor;
+	}
+
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	}
